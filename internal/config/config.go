a98a35033ac9431fcaf537c504267c6fd06bb1e0
4madness7 <marcomirizzi4@gmail.com>
2024-12-20 15:35:45 +0100

    fix bug on writing to config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,57 +10,50 @@ import (
 const configFileName = ".gatorconfig.json"
 
 func getConfigFilePath() (string, error) {
-    homeDir, err := os.UserHomeDir()
-    if err != nil {
-        return "", fmt.Errorf("Could not find $HOME: %w", err)
-    }
-    path := filepath.Join(homeDir, configFileName)
-    return path, nil
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("Could not find $HOME: %w", err)
+	}
+	path := filepath.Join(homeDir, configFileName)
+	return path, nil
 }
 
 func write(cfg Config) error {
-    filePath, err := getConfigFilePath()
-    if err != nil {
-        return fmt.Errorf("Could not write to file: %w", err)
-    }
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return fmt.Errorf("Could not write to file: %w", err)
+	}
 
-    f, err := os.OpenFile(filePath, os.O_WRONLY, 0644)
-    if err != nil {
-        return fmt.Errorf("Could not open file: %w", err)
-    }
-    defer f.Close()
+	f, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("Could not open file: %w", err)
+	}
+	defer f.Close()
 
-    data, err := json.Marshal(cfg)
-    if err != nil {
-        return fmt.Errorf("Could not Marshal config: %w", err)
-    }
-
-    _, err = f.Write(data)
-    if err != nil {
-        return fmt.Errorf("Something went wrong when saving file: %w", err)
-    }
-
-    return nil
+	encoder := json.NewEncoder(f)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func Read() (Config, error) {
-    filePath, err := getConfigFilePath()
-    if err != nil {
-        return Config{}, err
-    }
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
 
-    data, err := os.ReadFile(filePath)
-    if err != nil {
-        return Config{}, err
-    }
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, err
+	}
 
-    var config Config
-    err = json.Unmarshal(data, &config)
-    if err != nil {
-        return Config{}, fmt.Errorf("Could not read config file: %w", err)
-    }
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return Config{}, fmt.Errorf("Could not read config file: %w", err)
+	}
 
-    return config, nil
+	return config, nil
 }
-
-
